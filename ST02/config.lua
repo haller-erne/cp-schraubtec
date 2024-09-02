a3f4dca63cfb166a19da84f6ee33350de55642b4
ogsadmin <ogsadmin@haller-erne.de>
2024-09-02 19:25:36 +0200

    Added Verpose tracking

--- ST02/config.lua
+++ ST02/config.lua
@@ -17,7 +17,8 @@ requires = {
 	--"station_io",
 	-- "lua_tool_mqtt",
 	"sim",
-	"web_workflow"
+	"web_workflow",
+	"station_io"			-- Verpose
 }
 current_project.logo_file = 'logo-gwk.png'
 current_project.billboard = 'http://127.0.0.1:59990/billboard.html'
